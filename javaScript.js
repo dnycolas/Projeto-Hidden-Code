8a153995d7a42fd752477f2b4bf30300fc71e79d
dnycolas <dnycolas088@gmail.com>
2024-05-17 17:28:34 -0300

    feat: implementado alguma ideias javascript

---
+++ javaScript.js
@@ -0,0 +1,9 @@
+// fazer menu de hamburger(header)
+// fazer botão para selecionar os cases (cases)
+    // 1. mudar o nome de alguns elementos para ficar mais facil de localizar
+    // 2. fazer um programar que mude as imagens e os textos 
+    // 3. colocar essa função ao botão
+
+
+// implementar sistemas API nos input (contate-nos)
+// implementar sistema API nos input (feedback)  
