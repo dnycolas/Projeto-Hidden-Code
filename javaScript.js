6f22f1e13514df48ccb5cf882ad292533e62c690
dnycolas <dnycolas088@gmail.com>
2024-05-20 12:27:56 -0300

    feat: adicionado javascript

--- javaScript.js
+++ javaScript.js
@@ -2,6 +2,34 @@
 // fazer botão para selecionar os cases (cases)
     // 1. mudar o nome de alguns elementos para ficar mais facil de localizar
     // 2. fazer um programar que mude as imagens e os textos 
+
+
+    // Eventos para os botoes
+    
+
+    
+    // funcoes
+  const button = document.getElementById("botao");
+  const div = document.getElementById('oncinha');
+
+  // definir a funcao que vai nos elementos
+  function mudarOncinha() {
+    oncinha.innerHTML = '<p>Reduzimos o desperdício de comida em uma escola em </p>';
+  }
+
+  button.addEventListener(mudarOncinha);
+        
+    // adicionar evento no butao
+    
+    
+            
+
+        
+        
+
+
+
+
     // 3. colocar essa função ao botão
 
 
